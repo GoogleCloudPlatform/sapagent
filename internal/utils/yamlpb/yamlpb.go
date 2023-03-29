014a65b5cce0bebcbd3182be7395aa965d8fb688
Raghav Sharma <raghavsharmaa@google.com>
2023-03-29 08:40:36 -0700

    Internal change
    PiperOrigin-RevId: 520345992

---
+++ internal/utils/yamlpb/yamlpb.go
@@ -0,0 +1,103 @@
+/*
+Copyright 2023 Google LLC
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    https://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+// Package yamlpb contains util methods to unmarshall a YAML string to a proto message
+package yamlpb
+
+import (
+	"encoding/json"
+	"fmt"
+	"strconv"
+
+	"google.golang.org/protobuf/encoding/protojson"
+	"google.golang.org/protobuf/proto"
+	"google3/third_party/golang/yaml/yaml"
+)
+
+// toJSONCompatibleValue recursively converts the YAML-compatible value to a
+// JSON-comptaible value if possible, else returns an error.
+func toJSONCompatibleValue(i any) (o any, err error) {
+	switch t := i.(type) {
+	case map[any]any:
+		return toStrMap(t)
+	case []byte:
+		return t, nil
+	case []any:
+		var arr []any
+		for _, v := range t {
+			var j any
+			j, err = toJSONCompatibleValue(v)
+			if err != nil {
+				return nil, err
+			}
+			arr = append(arr, j)
+		}
+		return arr, nil
+	}
+	return i, nil
+}
+
+// toStrMap creates a string-indexed map based on the provided map if possible.
+// YAML supports many different kinds of indices in maps. JSON only supports
+// string-indexed maps and protobuf only supports string and integer-indexed
+// maps. So, we convert integer-indexed maps to string maps according to
+// https://developers.google.com/protocol-buffers/docs/proto3#json
+// and recursively convert values to JSON-compatible types.
+func toStrMap(i map[any]any) (o map[string]any, err error) {
+	o = make(map[string]any)
+	for k, v := range i {
+		var s string
+		switch t := k.(type) {
+		case string:
+			s = t
+		case int:
+			s = strconv.FormatInt(int64(t), 10)
+		default:
+			err = fmt.Errorf("'%v' is not a string or integer. Only string and integer keys are supported", k)
+			return nil, err
+		}
+		o[s], err = toJSONCompatibleValue(v)
+		if err != nil {
+			return nil, err
+		}
+	}
+	return o, nil
+}
+
+// UnmarshalString will populate the fields of a protocol buffer based on a YAML
+// string.
+func UnmarshalString(s string, pb proto.Message) error {
+	var m any
+	if err := yaml.UnmarshalStrict([]byte(s), &m); err != nil {
+		return fmt.Errorf("cannot unmarshal yaml string: %v", err)
+	}
+
+	v, err := toJSONCompatibleValue(m)
+	if err != nil {
+		return err
+	}
+
+	var j []byte
+	if j, err = json.Marshal(v); err != nil {
+		return err
+	}
+
+	if err := protojson.Unmarshal(j, pb); err != nil {
+		return err
+	}
+
+	return nil
+}
---
+++ internal/utils/yamlpb/yamlpb_test.go
@@ -0,0 +1,194 @@
+/*
+Copyright 2023 Google LLC
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    https://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package yamlpb
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+	"github.com/google/go-cmp/cmp/cmpopts"
+	"google.golang.org/protobuf/proto"
+	test_pb "github.com/GoogleCloudPlatform/sapagent/protos/yamlpbtest"
+)
+
+var (
+	defaultYAML string = `
+uint32_v: 10
+float_v: 0.3
+string_v: hello
+bool_v: false
+enum_v: VAL_ONE
+nested_v:
+  uint32_v: 1
+uint32_r:
+- 5
+- 10
+- 20
+nested_r:
+- uint32_v: 5
+- uint32_v: 10
+- uint32_v: 20
+`
+
+	nestedYAML string = `
+nested_r:
+- &anchor_nested_r
+  uint32_v: 5
+- uint32_v: 7
+- *anchor_nested_r
+`
+
+	aliasYAML string = `
+uint32_r:
+- &num_alias 10
+- 5
+- *num_alias
+`
+
+	duplicateEntryYAML = `
+uint32_v: 11
+uint32_v: 12
+`
+)
+
+func TestToJSONCompatibilityView(t *testing.T) {
+	tests := []struct {
+		name    string
+		input   map[any]any
+		want    map[string]any
+		wantErr error
+	}{
+		{
+			name: "ValidInput",
+			input: map[any]any{
+				"str_key": "str_value",
+				"int_key": 10,
+				5:         20.5,
+				20:        []int{1, 2, 3},
+				"dict": map[any]any{
+					5: 10,
+				},
+			},
+			want: map[string]any{
+				"str_key": "str_value",
+				"int_key": 10,
+				"5":       20.5,
+				"20":      []int{1, 2, 3},
+				"dict": map[string]any{
+					"5": 10,
+				},
+			},
+			wantErr: nil,
+		},
+		{
+			name:    "ToStrMapRejectsInvalidKey",
+			input:   map[any]any{5.5: "this is messed up"},
+			want:    nil,
+			wantErr: cmpopts.AnyError,
+		},
+		{
+			name: "ToStrMapRejectsInvalidKeyInNestedMap",
+			input: map[any]any{
+				"dict": map[any]any{
+					5.5: 10,
+				},
+			},
+			want:    nil,
+			wantErr: cmpopts.AnyError,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			got, err := toJSONCompatibleValue(test.input)
+			if !reflect.DeepEqual(got, test.want) {
+				t.Errorf("Test: %s toJSONCompatibleView() (-want %v +got %v):\n", test.name, test.want, got)
+			}
+			if test.wantErr != nil && !cmp.Equal(err, test.wantErr, cmpopts.EquateErrors()) {
+				t.Errorf("Test: %s toJSONCompatibleView() (-want %v +got %v):\n", test.name, test.wantErr, err)
+			}
+		})
+	}
+}
+
+func TestUnmarshalString(t *testing.T) {
+	tests := []struct {
+		name    string
+		input   string
+		want    *test_pb.TestMessage
+		wantErr error
+	}{
+		{
+			name:  "ValidInput",
+			input: defaultYAML,
+			want: &test_pb.TestMessage{
+				Uint32V: 10,
+				FloatV:  0.3,
+				StringV: "hello",
+				BoolV:   false,
+				EnumV:   test_pb.TestEnum_VAL_ONE,
+				NestedV: &test_pb.NestedTestMessage{Uint32V: 1},
+				Uint32R: []uint32{5, 10, 20},
+				NestedR: []*test_pb.NestedTestMessage{
+					{Uint32V: 5},
+					{Uint32V: 10},
+					{Uint32V: 20},
+				},
+			},
+			wantErr: nil,
+		},
+		{
+			name:  "ValidInputWithNested",
+			input: nestedYAML,
+			want: &test_pb.TestMessage{
+				NestedR: []*test_pb.NestedTestMessage{
+					{Uint32V: 5},
+					{Uint32V: 7},
+					{Uint32V: 5},
+				},
+			},
+		},
+		{
+			name:  "InputWithAlias",
+			input: aliasYAML,
+			want: &test_pb.TestMessage{
+				Uint32R: []uint32{10, 5, 10},
+			},
+			wantErr: nil,
+		},
+		{
+			name:    "InvalidInput",
+			input:   duplicateEntryYAML,
+			want:    &test_pb.TestMessage{},
+			wantErr: cmpopts.AnyError,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			tpb := &test_pb.TestMessage{}
+			gotErr := UnmarshalString(test.input, tpb)
+			if !proto.Equal(tpb, test.want) {
+				t.Errorf("Test: %s Unmarshal() (-want %v, +got %v):\n", test.name, test.want, tpb)
+			}
+			if gotErr != nil && !cmp.Equal(gotErr, test.wantErr, cmpopts.EquateErrors()) {
+				t.Errorf("Test: %s Unmarshal() (-want %v, +got %v)", test.name, test.wantErr, gotErr)
+			}
+		})
+	}
+}
