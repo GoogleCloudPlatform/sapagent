751021519302f022aeba215b38bb08a5d191437b
Zachary Zabetakis <zsz@google.com>
2023-07-24 13:26:49 -0700

    No public description
    PiperOrigin-RevId: 550656138

--- internal/configuration/configuration.go
+++ internal/configuration/configuration.go
@@ -59,7 +59,7 @@ const (
 
 	// AgentVersion is the version of the agent.
 	AgentVersion = `2.3`
-	// LINT.ThenChange(//depot/google3/third_party/sapagent/BUILD, //depot/google3/third_party/sapagent/google-cloud-sap-agent.blueprint)
+	// LINT.ThenChange(//depot/google3/third_party/sapagent/BUILD, //depot/google3/third_party/sapagent/google-cloud-sap-agent.blueprint, //depot/google3/cloud/hosted/workloadmanager/rules/constraints/resources/agent_updated_on_sap_vm.go)
 
 	// LinuxConfigPath is the default path to agent configuration file on linux.
 	LinuxConfigPath = `/etc/google-cloud-sap-agent/configuration.json`
