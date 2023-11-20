0e332824b45c8656000550daed8ff9eaf877d035
Pavan Karkun <pkarkun@google.com>
2023-11-20 10:14:32 -0800

    Bump agent version to 2.8 to support C3/M3 certification
    PiperOrigin-RevId: 584060608

--- internal/configuration/configuration.go
+++ internal/configuration/configuration.go
@@ -58,7 +58,7 @@ const (
 	
 
 	// AgentVersion is the version of the agent.
-	AgentVersion = `2.7`
+	AgentVersion = `2.8`
 	
 
 	// LinuxConfigPath is the default path to agent configuration file on linux.
