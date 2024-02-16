797f18c03a8d974038a95b2e337830f6baf2226c
Jon Kenneson <kenneson@google.com>
2024-02-16 07:26:24 -0800

    Add helper functions to configureinstance OTE.
    PiperOrigin-RevId: 607683612

--- internal/onetime/configureinstance/configureinstance.go
+++ internal/onetime/configureinstance/configureinstance.go
@@ -19,9 +19,12 @@ limitations under the License.
 package configureinstance
 
 import (
+	"bytes"
 	"context"
+	"errors"
 	"fmt"
 	"os"
+	"regexp"
 	"strings"
 
 	"flag"
@@ -129,40 +132,160 @@ func (c *ConfigureInstance) Execute(ctx context.Context, f *flag.FlagSet, args .
 	c.writeFile = os.WriteFile
 	c.readFile = os.ReadFile
 	c.execute = commandlineexecutor.ExecuteCommand
-	if err := c.configureInstanceHandler(ctx); err != nil {
+	exitStatus, err := c.configureInstanceHandler(ctx)
+	if err != nil {
 		fmt.Println("ConfigureInstance: FAILED, detailed logs are at /var/log/google-cloud-sap-agent/configureinstance.log, err: ", err)
 		log.CtxLogger(ctx).Errorw("ConfigureInstance failed", "err", err)
 		usagemetrics.Error(usagemetrics.ConfigureInstanceFailure)
-		return subcommands.ExitFailure
 	}
-	return subcommands.ExitSuccess
+	return exitStatus
 }
 
 // configureInstanceHandler checks and applies OS settings
 // depending on the machine type.
-func (c *ConfigureInstance) configureInstanceHandler(ctx context.Context) error {
+func (c *ConfigureInstance) configureInstanceHandler(ctx context.Context) (subcommands.ExitStatus, error) {
 	LogToBoth(ctx, "ConfigureInstance starting")
 	usagemetrics.Action(usagemetrics.ConfigureInstanceStarted)
 	rebootRequired := false
 
-	LogToBoth(ctx, fmt.Sprintf("Using machine type: %s", c.machineType))
+	log.CtxLogger(ctx).Infof("Using machine type: %s", c.machineType)
 	switch {
 	// TODO: Add support for X4 customizations
 	case strings.HasPrefix(c.machineType, "x4"):
 	default:
-		return fmt.Errorf("unsupported machine type: %s", c.machineType)
+		return subcommands.ExitUsageError, fmt.Errorf("unsupported machine type: %s", c.machineType)
 	}
 
-	LogToBoth(ctx, "ConfigureInstance: SUCCESS, detailed logs are at /var/log/google-cloud-sap-agent/configureinstance.log")
 	usagemetrics.Action(usagemetrics.ConfigureInstanceFinished)
-	if rebootRequired {
-		LogToBoth(ctx, "\nReboot required")
+	exitStatus := subcommands.ExitSuccess
+	if c.apply || (c.check && !rebootRequired) {
+		LogToBoth(ctx, "ConfigureInstance: SUCCESS")
 	}
-	return nil
+	if c.apply && rebootRequired {
+		LogToBoth(ctx, "\nPlease note that a reboot is required for the changes to take effect.")
+	}
+	if c.check && rebootRequired {
+		LogToBoth(ctx, "ConfigureInstance: Your system configuration doesn't match best practice for your instance type. Please run 'configureinstance -apply' to fix.")
+		exitStatus = subcommands.ExitFailure
+	}
+	LogToBoth(ctx, "\nDetailed logs are at /var/log/google-cloud-sap-agent/configureinstance.log")
+	return exitStatus, nil
 }
 
 // LogToBoth prints to the console and writes an INFO msg to the log file.
 func LogToBoth(ctx context.Context, msg string) {
 	fmt.Println(msg)
 	log.CtxLogger(ctx).Info(msg)
+}
+
+// checkAndRegenerateFile verifies the contents of filePath and regenerates the
+// entire file if it is out of date. Returns true if the file is regenerated.
+func (c *ConfigureInstance) checkAndRegenerateFile(ctx context.Context, filePath string, want []byte) (bool, error) {
+	got, err := c.readFile(filePath)
+	if err != nil && !errors.Is(err, os.ErrNotExist) {
+		return false, err
+	}
+	if errors.Is(err, os.ErrNotExist) || !bytes.Equal(got, want) {
+		log.CtxLogger(ctx).Infow("File is out of date.", "filePath", filePath, "got", string(got), "want", string(want))
+		if c.check {
+			log.CtxLogger(ctx).Infof("To regenerate %s, run 'configureinstance -apply'.", filePath)
+		} else {
+			log.CtxLogger(ctx).Infof("Regenerating %s.", filePath)
+			if err := c.writeFile(filePath, want, 0644); err != nil {
+				return false, err
+			}
+		}
+		return true, nil
+	}
+	log.CtxLogger(ctx).Infof("%s is up to date", filePath)
+	return false, nil
+}
+
+// checkAndRegenerateLines verifies lines of filePath and regenerates the
+// lines if they are out of date. Returns true if any line is regenerated.
+// wantLines should be formatted as either a single key/value: 'key=value',
+// or multiple values for one key: 'key="value1 value2 value3"'.
+func (c *ConfigureInstance) checkAndRegenerateLines(ctx context.Context, filePath string, wantLines []string) (bool, error) {
+	fileLines, err := c.readFile(filePath)
+	if err != nil {
+		return false, err
+	}
+	regenerate := false
+	gotLines := strings.Split(string(fileLines), "\n")
+	for _, want := range wantLines {
+		key := strings.Split(want, "=")[0]
+		found := false
+		for i, got := range gotLines {
+			// The line in the file may be commented out, or have an improper value.
+			// Check if the key is anywhere in the line before regenerating.
+			if strings.Contains(got, key) {
+				found = true
+				if got != want {
+					var updated bool
+					if updated, gotLines[i] = regenerateLine(ctx, got, want); updated == true {
+						log.CtxLogger(ctx).Infof("%s is out of date. Got: '%s', want: '%s'", filePath, got, gotLines[i])
+						regenerate = true
+					}
+				}
+			}
+		}
+		// If the key is missing entirely, add it to the end of the file.
+		if !found {
+			log.CtxLogger(ctx).Infof("%s is out of date. Missing line: '%s'", filePath, want)
+			gotLines = append(gotLines, want+"\n")
+			regenerate = true
+		}
+	}
+
+	if regenerate {
+		if c.check {
+			log.CtxLogger(ctx).Infof("To regenerate %s, run 'configureinstance -apply'.", filePath)
+		} else {
+			log.CtxLogger(ctx).Infof("Regenerating %s.", filePath)
+			if err := c.writeFile(filePath, []byte(strings.Join(gotLines, "\n")), 0644); err != nil {
+				return false, err
+			}
+		}
+		return true, nil
+	}
+
+	log.CtxLogger(ctx).Infof("%s is up to date", filePath)
+	return false, nil
+}
+
+// regenerateLine will override values in 'got' provided by 'want,' while
+// preserving any original values not present in 'want'.
+// 'want' should be formatted as either a single key/value: 'key=value',
+// or multiple values for one key: 'key="value1 value2 value3"'.
+// Returns true if a substitution occurred.
+func regenerateLine(ctx context.Context, got, want string) (bool, string) {
+	if got == want {
+		return false, got
+	}
+	// Single value, just replace 'got' with 'want'.
+	if !strings.Contains(got, " ") && !strings.Contains(want, " ") {
+		return true, want
+	}
+	// Multiple values will iterate through each, applying changes if necessary.
+	split := strings.SplitN(want, "=", 2)
+	if len(split) != 2 {
+		log.CtxLogger(ctx).Errorf("Invalid format for want: '%s'", want)
+		return false, got
+	}
+	values := strings.Trim(split[1], `"`)
+	updated := false
+	for _, val := range strings.Split(values, " ") {
+		if !strings.Contains(got, val) {
+			updated = true
+		}
+		// Values will overwrite existing occurrences in 'got'.
+		key := strings.Split(val, "=")[0]
+		re := regexp.MustCompile(key + `[^\s\"]*`)
+		got = re.ReplaceAllString(got, val)
+		// If not found, append to the end of 'got'.
+		if !strings.Contains(got, val) {
+			got = strings.TrimSuffix(got, `"`) + fmt.Sprintf(` %s"`, val)
+		}
+	}
+	return updated, got
 }
--- internal/onetime/configureinstance/configureinstance_test.go
+++ internal/onetime/configureinstance/configureinstance_test.go
@@ -18,6 +18,8 @@ package configureinstance
 
 import (
 	"context"
+	"fmt"
+	"os"
 	"testing"
 
 	"flag"
@@ -25,8 +27,48 @@ import (
 	"github.com/google/go-cmp/cmp/cmpopts"
 	"github.com/google/subcommands"
 	ipb "github.com/GoogleCloudPlatform/sapagent/protos/instanceinfo"
+	"github.com/GoogleCloudPlatform/sapagent/shared/commandlineexecutor"
 	"github.com/GoogleCloudPlatform/sapagent/shared/log"
 )
+
+// defaultWriteFile will return nil error up to numNil,
+// after which they will return an error.
+func defaultWriteFile(numNil int) func(string, []byte, os.FileMode) error {
+	return func(string, []byte, os.FileMode) error {
+		if numNil > 0 {
+			numNil--
+			return nil
+		}
+		return cmpopts.AnyError
+	}
+}
+
+// The following funcs will return the specified exit code and std
+// out each run. Note: The slices should be equal length.
+func defaultReadFile(errors []error, contents []string) func(string) ([]byte, error) {
+	i := 0
+	return func(string) ([]byte, error) {
+		if i >= len(errors) || i >= len(contents) {
+			i = 0
+		}
+		content := []byte(contents[i])
+		error := errors[i]
+		i++
+		return content, error
+	}
+}
+
+func defaultExecute(exitCodes []int, stdOuts []string) func(context.Context, commandlineexecutor.Params) commandlineexecutor.Result {
+	i := 0
+	return func(context.Context, commandlineexecutor.Params) commandlineexecutor.Result {
+		if i >= len(exitCodes) || i >= len(stdOuts) {
+			i = 0
+		}
+		result := commandlineexecutor.Result{ExitCode: exitCodes[i], StdOut: stdOuts[i]}
+		i++
+		return result
+	}
+}
 
 func TestExecuteConfigureInstance(t *testing.T) {
 	tests := []struct {
@@ -107,23 +149,10 @@ func TestExecuteConfigureInstance(t *testing.T) {
 		},
 		{
 			name: "UnsupportedMachineType",
-			want: subcommands.ExitFailure,
+			want: subcommands.ExitUsageError,
 			c: ConfigureInstance{
 				apply:       true,
 				machineType: "",
-			},
-			args: []any{
-				"test",
-				log.Parameters{},
-				&ipb.CloudProperties{},
-			},
-		},
-		{
-			name: "X4MachineType",
-			want: subcommands.ExitSuccess,
-			c: ConfigureInstance{
-				apply:       true,
-				machineType: "x4-megamem-1440-metal",
 			},
 			args: []any{
 				"test",
@@ -166,30 +195,288 @@ func TestSetFlagsForConfigureInstance(t *testing.T) {
 
 func TestConfigureInstanceHandler(t *testing.T) {
 	tests := []struct {
-		name string
-		c    ConfigureInstance
-		want error
+		name    string
+		c       ConfigureInstance
+		want    subcommands.ExitStatus
+		wantErr error
 	}{
 		{
 			name: "UnsupportedMachineType",
 			c: ConfigureInstance{
 				machineType: "",
 			},
-			want: cmpopts.AnyError,
+			want:    subcommands.ExitUsageError,
+			wantErr: cmpopts.AnyError,
 		},
 		{
-			name: "X4MachineType",
+			name: "x4SuccessApply",
 			c: ConfigureInstance{
-				machineType: "x4-megamem-1440-metal",
+				machineType: "x4-megamem-1920",
+				apply:       true,
 			},
-			want: nil,
+			want:    subcommands.ExitSuccess,
+			wantErr: nil,
+		},
+		{
+			name: "x4SuccessCheck",
+			c: ConfigureInstance{
+				machineType: "x4-megamem-1920",
+				apply:       true,
+			},
+			want:    subcommands.ExitSuccess,
+			wantErr: nil,
 		},
 	}
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
-			got := test.c.configureInstanceHandler(context.Background())
-			if !cmp.Equal(got, test.want, cmpopts.EquateErrors()) {
+			got, gotErr := test.c.configureInstanceHandler(context.Background())
+			if !cmp.Equal(gotErr, test.wantErr, cmpopts.EquateErrors()) {
+				t.Errorf("configureInstanceHandler()=%v want %v", gotErr, test.wantErr)
+			}
+			if got != test.want {
 				t.Errorf("configureInstanceHandler()=%v want %v", got, test.want)
+			}
+		})
+	}
+}
+
+func TestCheckAndRegenerateFile(t *testing.T) {
+	tests := []struct {
+		name            string
+		c               ConfigureInstance
+		wantRegenerated bool
+		wantErr         error
+	}{
+		{
+			name: "FailedToRead",
+			c: ConfigureInstance{
+				readFile: func(string) ([]byte, error) { return nil, fmt.Errorf("failed to read file") },
+			},
+			wantRegenerated: false,
+			wantErr:         cmpopts.AnyError,
+		},
+		{
+			name: "OutOfDateFileWithCheck",
+			c: ConfigureInstance{
+				readFile:  defaultReadFile([]error{nil}, []string{"key=wrong_value"}),
+				writeFile: defaultWriteFile(1),
+				check:     true,
+			},
+			wantRegenerated: true,
+			wantErr:         nil,
+		},
+		{
+			name: "OutOfDateFileWithApplyFailedToWrite",
+			c: ConfigureInstance{
+				readFile:  defaultReadFile([]error{nil}, []string{"key=wrong_value"}),
+				writeFile: defaultWriteFile(0),
+				apply:     true,
+			},
+			wantRegenerated: false,
+			wantErr:         cmpopts.AnyError,
+		},
+		{
+			name: "OutOfDateFileWithApplySuccessfulWrite",
+			c: ConfigureInstance{
+				readFile:  defaultReadFile([]error{nil}, []string{"key=wrong_value"}),
+				writeFile: defaultWriteFile(1),
+				apply:     true,
+			},
+			wantRegenerated: true,
+			wantErr:         nil,
+		},
+		{
+			name: "NoUpdatesNeeded",
+			c: ConfigureInstance{
+				readFile: defaultReadFile([]error{nil}, []string{"key=value"}),
+			},
+			wantRegenerated: false,
+			wantErr:         nil,
+		},
+	}
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			got, gotErr := tc.c.checkAndRegenerateFile(context.Background(), "", []byte("key=value"))
+			if !cmp.Equal(gotErr, tc.wantErr, cmpopts.EquateErrors()) {
+				t.Errorf("checkAndRegenerateFile(%#v) returned error: %v, want error: %v", tc.c, gotErr, tc.wantErr)
+			}
+			if got != tc.wantRegenerated {
+				t.Errorf("checkAndRegenerateFile(%#v) = %v, want: %v", tc.c, got, tc.wantRegenerated)
+			}
+		})
+	}
+}
+
+func TestCheckAndRegenerateLines(t *testing.T) {
+	tests := []struct {
+		name            string
+		c               ConfigureInstance
+		wantLines       []string
+		wantRegenerated bool
+		wantErr         error
+	}{
+		{
+			name: "ReadFileFailure",
+			c: ConfigureInstance{
+				readFile: func(string) ([]byte, error) { return nil, fmt.Errorf("failed to read file") },
+			},
+			wantLines:       []string{""},
+			wantRegenerated: false,
+			wantErr:         cmpopts.AnyError,
+		},
+		{
+			name: "CommentedOutKey",
+			c: ConfigureInstance{
+				readFile:  defaultReadFile([]error{nil}, []string{"#key=value"}),
+				writeFile: defaultWriteFile(1),
+				apply:     true,
+			},
+			wantLines:       []string{"key=value"},
+			wantRegenerated: true,
+			wantErr:         nil,
+		},
+		{
+			name: "NewValueForKey",
+			c: ConfigureInstance{
+				readFile:  defaultReadFile([]error{nil}, []string{"key=1"}),
+				writeFile: defaultWriteFile(1),
+				apply:     true,
+			},
+			wantLines:       []string{"key=2"},
+			wantRegenerated: true,
+			wantErr:         nil,
+		},
+		{
+			name: "NoUpdatesNeeded",
+			c: ConfigureInstance{
+				readFile:  defaultReadFile([]error{nil}, []string{"key=1"}),
+				writeFile: defaultWriteFile(1),
+				apply:     true,
+			},
+			wantLines:       []string{"key=1"},
+			wantRegenerated: false,
+			wantErr:         nil,
+		},
+		{
+			name: "MultiValueForKey",
+			c: ConfigureInstance{
+				readFile:  defaultReadFile([]error{nil}, []string{`key="val test=1"`}),
+				writeFile: defaultWriteFile(1),
+				apply:     true,
+			},
+			wantLines:       []string{`key="test=2 new=3"`},
+			wantRegenerated: true,
+			wantErr:         nil,
+		},
+		{
+			name: "KeyNotFound",
+			c: ConfigureInstance{
+				readFile:  defaultReadFile([]error{nil}, []string{`key="val test=1"`}),
+				writeFile: defaultWriteFile(1),
+				check:     true,
+			},
+			wantLines:       []string{`another_key=value`},
+			wantRegenerated: true,
+			wantErr:         nil,
+		},
+		{
+			name: "FailedToWrite",
+			c: ConfigureInstance{
+				readFile:  defaultReadFile([]error{nil}, []string{`key="val test=1"`}),
+				writeFile: defaultWriteFile(0),
+				apply:     true,
+			},
+			wantLines:       []string{`another_key=value`},
+			wantRegenerated: false,
+			wantErr:         cmpopts.AnyError,
+		},
+	}
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			gotRegenerated, gotErr := tc.c.checkAndRegenerateLines(context.Background(), "", tc.wantLines)
+			if !cmp.Equal(gotErr, tc.wantErr, cmpopts.EquateErrors()) {
+				t.Errorf("checkAndRegenerateLines(%#v) returned error: %v, want error: %v", tc.c, gotErr, tc.wantErr)
+			}
+			if gotRegenerated != tc.wantRegenerated {
+				t.Errorf("checkAndRegenerateLines(%#v) = %v, want: %v", tc.c, gotRegenerated, tc.wantRegenerated)
+			}
+		})
+	}
+}
+
+func TestRegenerateLine(t *testing.T) {
+	tests := []struct {
+		name        string
+		gotLine     string
+		wantLine    string
+		wantUpdated bool
+		wantOutput  string
+	}{
+		{
+			name:        "SingleValueMatch",
+			gotLine:     "key=1",
+			wantLine:    "key=1",
+			wantUpdated: false,
+			wantOutput:  "key=1",
+		},
+		{
+			name:        "SingleValueMismatch",
+			gotLine:     "key=1",
+			wantLine:    "key=2",
+			wantUpdated: true,
+			wantOutput:  "key=2",
+		},
+		{
+			name:        "MultiValueMatch",
+			gotLine:     `key="val test=2"`,
+			wantLine:    `key="val test=2"`,
+			wantUpdated: false,
+			wantOutput:  `key="val test=2"`,
+		},
+		{
+			name:        "MultiValueMismatch",
+			gotLine:     `key="val=1 test=3"`,
+			wantLine:    `key="val=2 test=2"`,
+			wantUpdated: true,
+			wantOutput:  `key="val=2 test=2"`,
+		},
+		{
+			name:        "MultiValueMissingValue",
+			gotLine:     `key="val test=3"`,
+			wantLine:    `key="missing=2 another"`,
+			wantUpdated: true,
+			wantOutput:  `key="val test=3 missing=2 another"`,
+		},
+		{
+			name:        "MultiValueSingleValUpdate",
+			gotLine:     `key="val test=3"`,
+			wantLine:    `key=test=2`,
+			wantUpdated: true,
+			wantOutput:  `key="val test=2"`,
+		},
+		{
+			name:        "MultiValueSingleValAdded",
+			gotLine:     `key="val test=3"`,
+			wantLine:    `key=missing`,
+			wantUpdated: true,
+			wantOutput:  `key="val test=3 missing"`,
+		},
+		{
+			name:        "InvalidWantFormat",
+			gotLine:     `key="val test=3"`,
+			wantLine:    `key`,
+			wantUpdated: false,
+			wantOutput:  `key="val test=3"`,
+		},
+	}
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			gotUpdated, gotOutput := regenerateLine(context.Background(), tc.gotLine, tc.wantLine)
+			if gotUpdated != tc.wantUpdated {
+				t.Errorf("regenerateLine(%q, %q) = %v, want: %v", tc.gotLine, tc.wantLine, gotUpdated, tc.wantUpdated)
+			}
+			if gotOutput != tc.wantOutput {
+				t.Errorf("regenerateLine(%q, %q) = %v, want: %v", tc.gotLine, tc.wantLine, gotOutput, tc.wantOutput)
 			}
 		})
 	}
