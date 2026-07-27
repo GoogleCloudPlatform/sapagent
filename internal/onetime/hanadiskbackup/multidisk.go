a4d306e8dcd534ce0db49a5f9618d597f372a3dc
Neejor Chakma <neejor@google.com>
2026-07-27 07:11:07 -0700

    Remove stale Instant Snapshot Group deletion from HANA multi-disk backup workflow.
    PiperOrigin-RevId: 954610724

--- internal/onetime/hanadiskbackup/multidisk.go
+++ internal/onetime/hanadiskbackup/multidisk.go
@@ -40,66 +40,7 @@ import (
 	ipb "github.com/GoogleCloudPlatform/sapagent/protos/instanceinfo"
 )
 
-func (s *Snapshot) deleteStaleISGs(ctx context.Context) error {
-	log.CtxLogger(ctx).Debug("Deleting stale instant snapshot groups")
-	isgList, err := s.isgService.ListInstantSnapshotGroups(ctx, s.Project, s.DiskZone)
-	if err != nil {
-		log.CtxLogger(ctx).Errorw("Error listing instant snapshot groups", "error", err)
-		return err
-	}
-
-	vmDisks := make(map[string]bool)
-	for _, d := range s.disks {
-		vmDisks[d] = true
-	}
-
-	var deleteErr error
-	for _, isg := range isgList {
-		if isg.Status != "READY" {
-			log.CtxLogger(ctx).Debugw("Instant snapshot group is not ready, skipping deletion", "ISG Name", isg.Name)
-			continue
-		}
-		cgParts := strings.Split(strings.TrimSuffix(isg.SourceConsistencyGroup, "/"), "/")
-		if cgParts[len(cgParts)-1] != s.cgName {
-			log.CtxLogger(ctx).Debugw("Instant snapshot group does not belong to target consistency group, skipping deletion", "ISG Name", isg.Name, "SourceConsistencyGroup", isg.SourceConsistencyGroup, "TargetConsistencyGroup", s.cgName)
-			continue
-		}
-		snapshots, err := s.isgService.DescribeInstantSnapshots(ctx, s.Project, s.DiskZone, isg.Name)
-		if err != nil {
-			log.CtxLogger(ctx).Warnw("Error describing instant snapshots for ISG, skipping deletion", "isg", isg.Name, "error", err)
-			continue
-		}
-
-		if len(snapshots) != len(s.disks) {
-			log.CtxLogger(ctx).Debugw("Instant snapshot group disk count does not match current VM disk count, skipping deletion", "ISG Name", isg.Name, "ISG Disk Count", len(snapshots), "VM Disk Count", len(s.disks))
-			continue
-		}
-
-		allDisksMatch := true
-		for _, is := range snapshots {
-			parts := strings.Split(is.SourceDisk, "/")
-			diskName := parts[len(parts)-1]
-			if !vmDisks[diskName] {
-				allDisksMatch = false
-				break
-			}
-		}
-		if !allDisksMatch {
-			log.CtxLogger(ctx).Debugw("Instant snapshot group contains snapshots for disks not currently attached to VM, skipping deletion", "ISG Name", isg.Name)
-			continue
-		}
-		if err = s.isgService.DeleteISG(ctx, s.Project, s.DiskZone, isg.Name); err != nil {
-			deleteErr = err
-			log.CtxLogger(ctx).Errorw("Error deleting instant snapshot group", "isg", isg.Name, "project", s.Project, "zone", s.DiskZone, "error", err)
-		}
-	}
-	return deleteErr
-}
-
 func (s *Snapshot) runWorkflowForInstantSnapshotGroups(ctx context.Context, run queryFunc, cp *ipb.CloudProperties) (err error) {
-	if err := s.deleteStaleISGs(ctx); err != nil {
-		log.CtxLogger(ctx).Errorw("Error deleting stale instant snapshot groups", "error", err)
-	}
 
 	for _, d := range s.disks {
 		if err = s.isDiskAttachedToInstance(ctx, d, cp); err != nil {
